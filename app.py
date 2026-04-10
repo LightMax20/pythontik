5c257b2d8ee45767f0c5de6d0b205d6c66b11949
LightMax20 <161189264+LightMax20@users.noreply.github.com>
2026-04-10 21:53:59 +0600

    Update app.py

--- app.py
+++ app.py
@@ -44,5 +44,8 @@ def download():
             os.remove(filename)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
